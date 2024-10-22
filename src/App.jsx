b243c6664113af55bcd7e6f8fea81e8c73fbfaf5
sassanghz <sassanghazi@gmail.com>
2024-10-22 19:59:34 -0400

    Title & Input box in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import List from '../component/List'
+import List from '../src/component/List'
 const App = () => {
   return (
     <div className='bg-stone-900 grid py-4 min-h-screen'>
