bcb90b90501edd953d79a2321a9194f422603fc8
sassanghz <sassanghazi@gmail.com>
2024-10-23 18:18:11 -0400

    delete icon

--- src/component/TodoItems.jsx
+++ src/component/TodoItems.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
+import delete_icon from '../assets/delete_icon.png'
 const TodoItems = () => {
   return (
     <div className='flex items-center my-3 gap-2'>
-      <div>
-        <img src={tick} alt=''></img>
-        <p>Operating Systems Midterm</p>
+      <div className='flex flex-1 items-center cursor-pointer'>
+        <img src={tick} alt='' className='w-7'></img>
+        <p className='tex-slate-700 ml-4 text-[17px]'>Operating Systems Midterm</p>
       </div>
+
+    <img src={delete_icon} alt='' className='2-3.5'></img>
+
+
     </div>
   )
 }
