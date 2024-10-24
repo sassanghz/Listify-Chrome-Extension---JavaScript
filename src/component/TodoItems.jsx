15ae54f48a7cbce3394ba1cb3636721d851baa7f
sassanghz <sassanghazi@gmail.com>
2024-10-24 01:18:21 -0400

    new inputText in List

--- src/component/TodoItems.jsx
+++ src/component/TodoItems.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete_icon.png'
-const TodoItems = () => {
+import text from 'body-parser/lib/types/text'
+const TodoItems = ({text}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
       <div className='flex flex-1 items-center cursor-pointer'>
         <img src={tick} alt='' className='w-7'></img>
-        <p className='tex-slate-700 ml-4 text-[17px]'>Operating Systems Midterm</p>
+        <p className='tex-slate-700 ml-4 text-[17px]'>{text}</p>
       </div>
 
-    <img src={delete_icon} alt='' className='2-3.5'></img>
+    <img src={delete_icon} alt='' className='w-3.5 cursor-pointer'></img>
 
 
     </div>
